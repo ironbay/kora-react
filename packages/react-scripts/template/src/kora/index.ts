ebaef85aef5bdc9cdaaeffb56ea162abf7793374
Dax Ravi <d@ironbay.co>
2017-11-26 17:13:49 -0500

    Installed kora

---
+++ packages/react-scripts/template/src/kora/index.ts
@@ -0,0 +1,2 @@
+import { Kora } from '@ironbay/kora'
+
--- packages/react-scripts/template/src/root/index.tsx
+++ packages/react-scripts/template/src/root/index.tsx
@@ -1,4 +1,3 @@
-import '@ironbay/legos/dist/legos.min.css'
 import * as React from 'react'
 
 export default class Root extends React.Component<any, any> {
